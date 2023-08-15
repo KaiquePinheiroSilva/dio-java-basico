248ecb8dfad67f2c148d90a7a5d6d62a5d89c756
LordGerald <forzazhori@gmail.com>
2023-08-14 22:18:35 -0300

    aula completa

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        //estudem a classe string que representa texto na aplicação
+
+        String meuNome = "Kaique Pinheiro";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 0;
+
+        System.out.println(numero);
+        
+        final double VALOR_DE_PI = 3.14; 
+        
+        // VALOR_DE_PI = 1;
     }
 }
