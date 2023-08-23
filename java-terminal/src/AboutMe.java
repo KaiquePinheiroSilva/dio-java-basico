d3d80cb6f038739f2aa091b42c0b61ae60d822c4
LordGerald <forzazhori@gmail.com>
2023-08-23 19:40:11 -0300

    atualização costumeira

---
+++ java-terminal/src/AboutMe.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        // criando scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        // //os argumentos começam com indice 0
+        // String nome = args [0];
+        // String sobrenome = args [1];
+        // int idade = Integer.valueOf(args[2]); //vamos falar sobre Wrappers
+        // double altura = Double.valueOf(args[3]);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade: ");
+        String idade = scanner.next();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+
+        // imprimindo os dados obtidos pelo usuário
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+    }
+}
