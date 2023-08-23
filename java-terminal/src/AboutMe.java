aca25272046ceea6d826fa2f33d0a6000b3231e2
LordGerald <forzazhori@gmail.com>
2023-08-23 20:31:34 -0300

    desafio concluido

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -29,5 +29,7 @@ public class AboutMe {
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + "cm ");
+
+        scanner.close();
     }
 }
